9726a34a2b6a4bc0f47443591e707932076f073e
massimiliano.balestrieri <massimiliano.balestrieri@gmail.com>
2021-04-24 16:47:58 +0200

    attempts

--- 2006-php-deskweb/src/deskweb5.php
+++ 2006-php-deskweb/src/deskweb5.php
@@ -40,5 +40,10 @@ if($_SERVER['REMOTE_ADDR']=="127.0.0.1" || $_SERVER['REMOTE_ADDR']=="10.0.0.10" 
 }else{
 	error_reporting(E_NONE);
 }
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("./project/deskweb-0.5/index.php");
 ?>
